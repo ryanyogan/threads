12b56e94c6d872a7033b5cb31a6970fc38586269
Ryan Yogan <ryanyogan@gmail.com>
2023-08-22 13:07:21 -0500

    feat // added asset images and first pass of top bar complete

--- src/components/shared/top-bar.tsx
+++ src/components/shared/top-bar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,6 +9,31 @@ export function TopBar() {
         <Image src="/logo.svg" alt="logo" height={28} width={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  height={24}
+                  width={24}
+                />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
     </nav>
   );
 }
